05365ec4c712862435712d2aec71d10e253614e0
Daniela <sunny.moon.amd2006@gmail.com>
2025-08-14 12:12:09 -0600

    Reparaciones
    Reparaciones errores

--- Tienda_vt/src/main/java/com/tienda/tienda/ProjectConfig.java
+++ Tienda_vt/src/main/java/com/tienda/tienda/ProjectConfig.java
@@ -1,9 +1,14 @@
 package com.tienda.tienda;
 
+import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
@@ -31,5 +36,26 @@ public class ProjectConfig implements WebMvcConfigurer{
         resolver.setOrder(0);
         resolver.setCheckExistence(true);
         return resolver;
+    }
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
     }
 }
